878f1c98e5681b17d092e5cdab21f058f806865c
Akira <vitorakira42@gmail.com>
2021-06-23 23:33:18 -0300

    Update 1
    Melhorias na interface e correção de pequenos bugs.

--- js/form.js
+++ js/form.js
@@ -1,43 +1,43 @@
 //máscara do cep
 $("#cepField").inputmask({"mask": "99999-999"});
 
-//formulario
-function btnEnviar() {
-    $("#form").submit(function(e) {
-        e.preventDefault();
-    });
-
+//busca cep
+function buscaCEP() {
+    //variável que armazena oque for inserido no campo cep
     var cep = document.getElementById("cepField").value;
+
     //remove traço da mascara
     cepst = cep.replace('-','');
 
-    var cepUrl = "https://viacep.com.br/ws/" + cepst + "/json/";
-
-    console.log(cepUrl)
-
-    var p = cep.includes("_")
+    //verifica se o campo possui caracteres vazios
+    var p = cep.includes("_");
 
     if (p == true) {
-        document.getElementById("erro").textContent="Preencha todos os números";
+        document.getElementById("custom-erro").textContent="Insira todos os números!";
+        document.getElementById("custom-consulta").classList.add('custom-consulta-hide');
     }
 
-    $.ajax({ 
-        url: cepUrl, 
-    });
+    //variavel 
+    var cepUrl = "https://viacep.com.br/ws/" + cepst + "/json/";
 
+    //faz a request 
     $.getJSON(cepUrl, function(data) {
-        // JSON result in `data` variable
         document.getElementById("cep").textContent=data.cep;
         document.getElementById("estado").textContent=data.uf;
         document.getElementById("cidade").textContent=data.localidade;
         document.getElementById("rua").textContent=data.logradouro;
-    
-        if (data.erro == true) {
-            document.getElementById("erro").textContent="CEP Inválido!";
-            document.getElementById("result").classList.add('result-hide');
-        } else {
-            document.getElementById("erro").textContent="";
-            document.getElementById("result").classList.remove('result-hide');
-        }
+
+        //verifica se houve erro no json
+        switch (data.erro) {
+            case true:
+                document.getElementById("custom-erro").textContent="CEP Inválido!"
+                document.getElementById("custom-consulta").classList.add('custom-consulta-hide');
+              break;
+        //se não houve erro, mostra os dados requisitados da json
+            default :
+                document.getElementById("custom-erro").textContent=""
+                document.getElementById("custom-consulta").classList.remove('custom-consulta-hide');
+              break;
+          }
     });
 }
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Olá :D! 
+ Para executar o projeto, basta abri-lo no github pages através do seguinte link: https://akira42.github.io/teste-petlove/ .
+ Caso o link não esteja disponível, basta baixar os arquivos do repositório, extrair o .zip, e abrir o arquivo index.html para testar as funcionalidades!
+
+Atenciosamente, 
+ Vitor Akira Nagahara
+
